8354b5d8da23c1f99a3ebb98d632aceb46063349
Jihyun Lee <jihyunlee.me@gmail.com>
2022-11-14 22:33:45 +0900

    initial commit

---
+++ nextjs-basic/pages/index.js
@@ -0,0 +1,19 @@
+import NavBar from '../components/NavBar'
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+/*
+1. 경로 설정하기
+1) /
+2) /about
+
+2. className 넣기
+3. useRouter사용해서 색상 변경하기
+4. 
+*/
