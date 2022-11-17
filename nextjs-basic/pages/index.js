1df780e63e67f7760e4372231479cc92bf3561eb
jihyunlee.me <jihyunlee.me@gmail.com>
2022-11-17 17:37:53 +0900

    refactor: 불필요한 주석 제거, eslint single quote를 double

--- nextjs-basic/pages/index.js
+++ nextjs-basic/pages/index.js
@@ -1,19 +1,9 @@
-import NavBar from '../components/NavBar'
+import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
       <NavBar />
       <h1>Home</h1>
     </div>
-  )
+  );
 }
-
-/*
-1. 경로 설정하기
-1) /
-2) /about
-
-2. className 넣기
-3. useRouter사용해서 색상 변경하기
-4. 
-*/
